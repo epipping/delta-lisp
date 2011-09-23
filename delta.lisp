90d1a572fcdbf68748712a212ae306522e7c55a8
Elias Pipping <pipping@exherbo.org>
2011-09-23 18:36:44 +0200

    Use bit-vectors

--- delta.lisp
+++ delta.lisp
@@ -10,17 +10,20 @@
 
 ;; FIXME: splits by newline at this point already
 
-(defvar *file-contents* (make-array 1 :adjustable t :fill-pointer 0))
+(defvar *file-contents* (make-array 0 :adjustable t :fill-pointer 0))
+(defvar *number-of-lines* 0)
 
 (defun read-file (filename)
   (with-open-file (stream filename)
     (loop for line = (read-line stream nil 'end)
           until (eq line 'end)
-          do (vector-push-extend line *file-contents*))))
+          do (vector-push-extend line *file-contents*)))
+  (setf *number-of-lines* (length *file-contents*)))
 
 (defun write-from-indices (indices stream)
-  (loop for i in indices
-        do (format stream "~a~%" (aref *file-contents* i))))
+  (loop for bit across indices
+        for index from 0
+        do (when (= 1 bit) (format stream "~a~%" (aref *file-contents* index)))))
 
 (defun indices->file (indices)
   (with-open-file (stream "output" :direction :output :if-exists :supersede)
@@ -36,31 +39,39 @@
   (floor (* part (/ length parts))))
 
 (defun test-complements (parts input)
-  (let ((len (length input)))
+  (let ((len (loop for x across input summing x)))
     (loop for i from 0 below parts
           for begin = (compute-break len i parts) then end
           and end = (compute-break len (1+ i) parts)
-          for complement = (append (subseq input 0 begin)
-                                   (subseq input end))
+          for length-removed = (- end begin)
+          for old-offset = 0 then new-offset
+          for new-offset = (loop for index from old-offset
+                                 with index-without-zeroes = begin
+                                 until (= index-without-zeroes end)
+                                 do (when (= 1 (aref input index))
+                                      (incf index-without-zeroes))
+                                 finally (return index))
+          for complement = (copy-seq input)
+          do (fill complement 0 :start old-offset :end new-offset)
           do (when (run-on-indices complement)
-               (format t "Reduced to ~a lines~%" (length complement))
+               (format t "Reduced to ~a lines~%" (- len length-removed))
                (osicat-posix:rename "output" "output-minimal")
-               (return complement)))))
+               (return (values complement (- len length-removed)))))))
 
 (defun delta (input)
-  (labels ((ddmin (list parts)
-             (let ((complement (test-complements parts list)))
-               (cond (complement (ddmin complement (max (1- parts) 2)))
+  (labels ((ddmin (list parts old-length)
+             (multiple-value-bind (complement new-length) (test-complements parts list)
+               (cond (complement (ddmin complement (max (1- parts) 2) new-length))
                      ;; check if increasing granularity makes sense
-                     ((< parts (length list)) (ddmin list (min (length list) (* 2 parts))))
+                     ((< parts old-length) (ddmin list (min old-length (* 2 parts)) old-length))
                      ;; done: found a 1-minimal subset
                      (t list)))))
     (if (run-on-indices input)
-        (format t "Starting with ~a lines~%" (length input))
+        (format t "Starting with ~a lines~%" *number-of-lines*)
         (error "Initial input does not satisfy the predicate"))
-    (ddmin input 2)))
+    (ddmin input 2 *number-of-lines*)))
 
 (defun delta-file (filename)
   (read-file filename)
-  (indices->file (delta (loop for i from 0 below (length *file-contents*)
-                              collect i))))
+  (indices->file (delta (make-array *number-of-lines* :element-type 'bit
+                                                      :initial-element 1))))

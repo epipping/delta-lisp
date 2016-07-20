68e69fa7849eef5f136ccdce11fd82110a4310cd
Elias Pipping <elias.pipping@fu-berlin.de>
2016-07-20 20:12:11 +0200

    test-soat: Make test case a bit more usable

--- test-soat/test.sh
+++ test-soat/test.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-/Applications/Xcode-beta.app/Contents//Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++ \
-    -x c++ -std=c++14 $1 2>&1 >/dev/null \
-    | grep -q 'Segmentation fault'
+# Fails at least with clang 3.7.1-3.8.1 (with assertions enabled!)
+CLANGXX=${CLANGXX:-clang++}
+
+out=`${CLANGXX} -x c++ -std=c++14 $1 2>&1 >/dev/null`
+[[ "x$out" == x*"Pack expansion without unexpanded packs"* ]]

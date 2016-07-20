0521017ba9b522d2b655f83a4e0ff3af9cd8266c
Elias Pipping <elias.pipping@fu-berlin.de>
2016-07-20 20:32:45 +0200

    test-silly: Do not shell out
    GNU grep (on Linux) is much slower than BSD grep (on macOS)

--- test-silly/test.sh
+++ test-silly/test.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-for ((i=1; i<100; ++i)); do
+n=100
+for ((i=1; i<n; ++i)); do
     [[ $i -eq 3 || $i -eq 7 || $i -eq 93 ]] && continue
-    # Intentionally slow
-    grep -q '^'$i'$' "$1" || exit -1
+    # Intentionally slow (quadratic in n)
+    found=false
+    while read line; do
+        [[ $line ==  $i ]] && found=true
+    done < $1
+    if ! ${found}; then
+        exit -1;
+    fi
 done

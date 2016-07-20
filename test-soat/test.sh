5fbdb80d069d0e1ac1eb8774648b331d7a3b1847
Elias Pipping <elias.pipping@fu-berlin.de>
2016-07-20 22:08:47 +0200

    test-soat: Update comment

--- test-soat/test.sh
+++ test-soat/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Fails at least with clang 3.7.1-3.8.1 (with assertions enabled!)
+# Fails at least with clang 3.6.2-3.8.1 (with assertions enabled!)
 CLANGXX=${CLANGXX:-clang++}
 
 out=`${CLANGXX} -x c++ -std=c++14 $1 2>&1 >/dev/null`

c0fa0853dc3c92999e7b673920a82d7a8145e81e
Elias Pipping <elias.pipping@fu-berlin.de>
2016-07-31 18:10:42 +0200

    Address an issue with ECL
    ECL still cannot be used at this point, but this is a further step
    towards full support.

--- mk/build.lisp
+++ mk/build.lisp
@@ -1,5 +1,9 @@
 (load "mk/path.lisp")
 
+;; Required by ECL, see e.g.
+;; https://mailman.common-lisp.net/pipermail/ecl-devel/2016-June/011120.html
+(asdf:register-immutable-system :uiop)
+
 (locally #+sbcl (declare (sb-ext:muffle-conditions
                          sb-kernel:redefinition-warning))
   (handler-bind (#+sbcl (sb-kernel:redefinition-warning #'muffle-warning))

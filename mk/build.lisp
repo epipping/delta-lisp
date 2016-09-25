51eb08df6de727976e5c9b0962ade0c8340ed570
Elias Pipping <elias.pipping@fu-berlin.de>
2016-09-25 12:31:27 +0000

    Remove ECL workaround

--- mk/build.lisp
+++ mk/build.lisp
@@ -1,9 +1,5 @@
 (load "mk/path.lisp")
 
-;; Required by ECL, see e.g.
-;; https://mailman.common-lisp.net/pipermail/ecl-devel/2016-June/011120.html
-(asdf:register-immutable-system :uiop)
-
 (asdf:operate 'asdf:program-op :delta-standalone)
 
 (quit)
